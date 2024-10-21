12507bc8a69bc7c696e0238ac14e33f6897662dc
aljoharah <j70725672@gmail.com>
2024-10-21 16:45:47 +0300

    Initial Commit

---
+++ hydrate2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hydrate2
+//
+//  Created by الجوهرة بنت عبدالله on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hydrate2/hydrate2App.swift
@@ -0,0 +1,17 @@
+//
+//  hydrate2App.swift
+//  hydrate2
+//
+//  Created by الجوهرة بنت عبدالله on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct hydrate2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
